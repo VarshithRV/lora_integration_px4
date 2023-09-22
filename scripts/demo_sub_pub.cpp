7a55f37f241afab7d9f4eedab228c976790d3e0a
Varshith R V <vrv278@gmail.com>
2023-09-23 00:10:55 +0530

    demo pub sub

--- scripts/demo_sub_pub.cpp
+++ scripts/demo_sub_pub.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include "px4_controller/key.h"
 #include <sstream>
+#include <unistd.h>
 
 px4_controller::key key_msg;
 
@@ -14,17 +15,21 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "talker");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<px4_controller::key>("chatter", 1000);
-  ros::Subscriber sub = n.subscribe("/demo_topic1", 1000, chatterCallback);
-  ros::Rate loop_rate(10);
+  ros::Publisher chatter = n.advertise<px4_controller::key>("chatter", 1000);
+  ros::Subscriber sub = n.subscribe("/keyboard", 1000, chatterCallback);
+  ros::Rate loop_rate(1);
+
   int count = 0;
   std_msgs::String string_msg;
+  
   while (ros::ok())
   {
-    // chatter_pub.publish(key_msg);
-    std::cout << key_msg.w << key_msg.a << key_msg.s << key_msg.d << std::endl;
     ros::spinOnce();
-    loop_rate.sleep();
+    chatter.publish(key_msg);
+    std::cout << key_msg.w << key_msg.a << key_msg.s << key_msg.d << std::endl;
+    // loop_rate.sleep();
+    // ros::spinOnce();
+    sleep(1);
   }
   return 0;
 }
